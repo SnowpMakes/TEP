0695eaf477d005b60917e61dd7536a1d6be65b8f
SnowpMakes <30631656+SnowpMakes@users.noreply.github.com>
2019-01-21 18:01:26 +0100

    Add basic argument parser. Is not implemented yet.

---
+++ src/io/snowp/tep/arguments/Argument.java
@@ -0,0 +1,11 @@
+package io.snowp.tep.arguments;
+
+/**
+ * An enum containing all possible arguments so they can be identified in the {@link java.util.HashMap} returned by {@link ArgumentParser#parse(String[])}
+ *
+ * @author Snowp
+ */
+public enum Argument
+{
+    FILE;
+}
